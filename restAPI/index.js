0ae8220db02e4e14f9fb945d4661f8bb0fe5f540
Florent TORNICELLI <florent@geode.com>
2023-05-03 10:55:28 +0200

    feat: add database connection

--- restAPI/index.js
+++ restAPI/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+const database = require('mongoose')
 const express = require("express")
 const app = express()
+
 
 app.use((req, res, next) => {
     res.on('finish', () => {
@@ -9,10 +12,22 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get("/", (req, res) => {
-    res.send({ success: true })
+
+app.get("/", (req, res) => {res.status(204)})
+
+
+app.get("/user", (req, res) => {
+    const schema = database.Schema({
+        field: { type: String, required: true },
+    })
+    database.model("users", schema).findById("64521d8db0e267ec82ebbd1f").then(user => res.send({ user }))
 })
 
-app.listen(8080, () => {
-    console.log("REST API is ready !\n")
-})
+
+database.connect(process.env.DB_CONNECTION)
+    .then(() => app.listen(8080, () => {
+        console.log("REST API is ready !\n")
+    }))
+    .catch(err => {
+        console.error(`Error during database connection process : ${err}`)
+    })
