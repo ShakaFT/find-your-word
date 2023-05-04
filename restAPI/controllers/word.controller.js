129f527848e4df09d8729923f2adb98939538e1c
Florent TORNICELLI <florent@geode.com>
2023-05-04 11:43:24 +0200

    feat: endpoints to get randomWord & check if word exists

---
+++ restAPI/controllers/word.controller.js
@@ -0,0 +1,50 @@
+const iso = require('iso-639-1');
+const Word = require('../models/word.model')
+const utils = require('../utils')
+
+function existsWord(req, res) {
+    const { lang, word } = req.body;
+
+    // Error handling
+    if (!iso.validate(lang)) {
+        utils.bad_request(res, "`lang` must follow ISO 3166-1 alpha-2 format.")
+        return
+    }
+    if (typeof word !== "string") {
+        utils.bad_request(res, "`word` should be String.")
+        return
+    }
+
+    const query = {}
+    query[`${lang}.text`] = word.toUpperCase()
+
+    Word.find(query)
+        .then(words => res.send({exists: words.length > 0}))
+}
+
+function randomWord(req, res) {
+    const { lang, length } = req.body;
+
+    // Error handling
+    if (!iso.validate(lang)) {
+        utils.bad_request(res, "`lang` must follow ISO 3166-1 alpha-2 format.")
+        return
+    }
+    if (!Number.isInteger(length) || length < 4) {
+        utils.bad_request(res, "`length` should be integer and greater than 4.")
+        return
+    }
+
+    const query = {}
+    query[`${lang}.length`] = length
+    const projection = { [`${lang}.text`]: 1 };
+
+    Word.find(query, projection)
+        .then(words => {
+            const randomWord = words[Math.floor(Math.random()*words.length)]
+            console.log(randomWord.get(lang))
+            res.send({word: randomWord.get(lang).text})
+        })
+}
+
+module.exports = { existsWord, randomWord }
--- restAPI/index.js
+++ restAPI/index.js
@@ -5,20 +5,22 @@ const express = require("express")
 const app = express()
 
 const userRouter = require("./routes/user.router")
+const wordRouter = require("./routes/word.router")
 
 app.use(bodyParser.json())
 
+app.get("/", (req, res) => {res.send()})
+
 app.use((req, res, next) => {
     res.on('finish', () => {
-        console.log(`${res.statusCode} ${req.method} ${req.url}`);
+        console.log(`${res.statusCode} ${req.method} ${req.baseUrl}${req.url}`);
         console.log(`    => ${res.statusMessage}\n`)
     });
     next();
 });
 
 app.use("/user", userRouter)
-
-app.get("/", (req, res) => {res.send()})
+app.use("/word", wordRouter)
 
 
 database.connect(process.env.DB_CONNECTION)
