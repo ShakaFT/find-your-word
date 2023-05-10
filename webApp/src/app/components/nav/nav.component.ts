459f9b775e51b8ff1ca931fdda6733bc8feca0bc
Carlos Vargas <varlopecar@gmail.com>
2023-05-10 15:15:05 +0200

    chore: menu options

--- webApp/src/app/components/nav/nav.component.ts
+++ webApp/src/app/components/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
+import { UserService } from "src/app/services/user.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-nav",
@@ -12,6 +14,20 @@ import { MatMenuModule } from "@angular/material/menu";
 })
 export class NavComponent {
   // @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  isLogged = false;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.isLogged = this.userService.isLogin();
+  }
+
+  ngOnInit(): void {
+    this.isLogged = this.userService.isLogin();
+  }
+
+  login() {
+    this.router.navigate(["/login"]);
+    console.log("login");
+  }
 
   // openMenu() {
   //   this.trigger.openMenu();
