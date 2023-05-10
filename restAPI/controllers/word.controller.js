117f70e4b086a8e1ed2415e6dae3cd61db7a4475
Florent TORNICELLI <florent@geode.com>
2023-05-10 15:03:54 +0200

    feat: add functional tests for word router

--- restAPI/controllers/word.controller.js
+++ restAPI/controllers/word.controller.js
@@ -1,16 +1,17 @@
-const iso = require('iso-639-1');
+const iso = require('iso-639-1')
 const Word = require('../models/word.model')
 const utils = require('../utils')
 
 function existsWord(req, res) {
-    const { lang, word } = req.query;
+    const wordRegex = /^[a-zA-Z]+$/
+    const { lang, word } = req.query
 
     // Error handling
     if (!iso.validate(lang)) {
         utils.bad_request(res, "`lang` must follow ISO 3166-1 alpha-2 format.")
         return
     }
-    if (typeof word !== "string") {
+    if (!wordRegex.test(word)) {
         utils.bad_request(res, "`word` should be String.")
         return
     }
@@ -19,38 +20,33 @@ function existsWord(req, res) {
     query[`${lang}.text`] = word.toUpperCase()
 
     Word.find(query)
-        .then(words => res.send({exists: words.length > 0}))
+        .then(words => res.send({ exists: words.length > 0 }))
         .catch(error => utils.internal_server(res, error))
 }
 
 function randomWord(req, res) {
-    let { lang, length } = req.query;
+    let { lang, length } = req.query
+    length = parseInt(length)
 
     // Error handling
     if (!iso.validate(lang)) {
         utils.bad_request(res, "`lang` must follow ISO 3166-1 alpha-2 format.")
         return
     }
-    try {
-        length = parseInt(length)
-        if (length < 4) {
-            throw new Error();
-        }
-    } catch (error) {
-        console.log(error)
+    if (isNaN(length) || length < 4) {
         utils.bad_request(res, "`length` should be integer and greater than 3.")
         return
     }
 
     const query = {}
     query[`${lang}.length`] = length
-    const projection = { [`${lang}.text`]: 1 };
+    const projection = { [`${lang}.text`]: 1 }
 
     Word.find(query, projection)
         .then(words => {
-            const randomWord = words[Math.floor(Math.random()*words.length)]
+            const randomWord = words[Math.floor(Math.random() * words.length)]
             console.log(randomWord.get(lang))
-            res.send({word: randomWord.get(lang).text})
+            res.send({ word: randomWord.get(lang).text })
         })
         .catch(error => utils.internal_server(res, error))
 }
