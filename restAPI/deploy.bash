8913067335544f957b7f3d0d62bfff1e1cb3a188
Florent TORNICELLI <florent@geode.com>
2023-05-10 08:46:20 +0200

    chore: quiet useless logs deployment

--- restAPI/deploy.bash
+++ restAPI/deploy.bash
@@ -1,5 +1,6 @@
 python3 -m venv .venv
-pip install -r requirements.txt
+pip install --upgrade pip -q -q -q
+pip install -r requirements.txt -q -q -q
 python python_scripts/deploy_documentation.py
 
 node index.js
