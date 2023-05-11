2785c537bb588a06cbbc0bdfe64578c8014c6092
Florent TORNICELLI <florent@geode.com>
2023-05-11 16:10:28 +0200

    fix: uncomment deploy script

--- restAPI/deploy.bash
+++ restAPI/deploy.bash
@@ -10,32 +10,32 @@ if [ -f ".env" ]; then
     export $(cat .env | xargs) >/dev/null 2>&1
 fi
 
-# echo "Will run tests of REST API"
-# npm run test
+echo "Will run tests of REST API"
+npm run test
 
-# TESTS_RESULT=`echo $?`
-# if [ $TESTS_RESULT != 0 ]; then
-#     echo "\nAn error occured during tests...\n"
-#     ./discord.bash "$DEPLOYMENT_DISCORD_WEBHOOK" "$TITLE" "$FAILED_DESCRIPTION" \
-#     "Unit tests failed. [Click here](https://dashboard.render.com/web/srv-ch95d16kobicv5rntc80/events) to see error details."
-#     exit 1
-# fi
+TESTS_RESULT=`echo $?`
+if [ $TESTS_RESULT != 0 ]; then
+    echo "\nAn error occured during tests...\n"
+    ./discord.bash "$DEPLOYMENT_DISCORD_WEBHOOK" "$TITLE" "$FAILED_DESCRIPTION" \
+    "Unit tests failed. [Click here](https://dashboard.render.com/web/srv-ch95d16kobicv5rntc80/events) to see error details."
+    exit 1
+fi
 
-# echo "\nAll tests passed!\n"
-# echo "\nWill deploy REST API Documentation\n"
+echo "\nAll tests passed!\n"
+echo "\nWill deploy REST API Documentation\n"
 
-# DOCUMENTATION_STATUS=`curl --write-out %{http_code} --silent --output /dev/null \
-#     -H "Authorization: Bearer $SWAGGER_TOKEN" -H "Content-Type: application/yaml" \
-#     --data-binary @documentation.yaml $SWAGGER_URL`
+DOCUMENTATION_STATUS=`curl --write-out %{http_code} --silent --output /dev/null \
+    -H "Authorization: Bearer $SWAGGER_TOKEN" -H "Content-Type: application/yaml" \
+    --data-binary @documentation.yaml $SWAGGER_URL`
 
-# if [ $DOCUMENTATION_STATUS -lt 200 ] || [ 299 -lt $DOCUMENTATION_STATUS ]; then
-#     echo "\nAn error occured during documentation deployment...\n"
-#     ./discord.bash "$DEPLOYMENT_DISCORD_WEBHOOK" "$TITLE" "$FAILED_DESCRIPTION" "Failed to deploy REST API documentation..."
-#     exit 1
-# fi
+if [ $DOCUMENTATION_STATUS -lt 200 ] || [ 299 -lt $DOCUMENTATION_STATUS ]; then
+    echo "\nAn error occured during documentation deployment...\n"
+    ./discord.bash "$DEPLOYMENT_DISCORD_WEBHOOK" "$TITLE" "$FAILED_DESCRIPTION" "Failed to deploy REST API documentation..."
+    exit 1
+fi
 
-# echo "\nDocumentation has been deployed!\n"
-# echo "\nWill deploy REST API\n"
+echo "\nDocumentation has been deployed!\n"
+echo "\nWill deploy REST API\n"
 
 ./discord.bash "$DEPLOYMENT_DISCORD_WEBHOOK" "$TITLE" "$SUCCESS_DESCRIPTION"
 
