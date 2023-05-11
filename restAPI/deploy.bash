a4eb9977509968e382bc4fb09dcdbe9bbfb6507f
Florent TORNICELLI <florent@geode.com>
2023-05-11 09:20:52 +0200

    fix: fix deploy script

--- restAPI/deploy.bash
+++ restAPI/deploy.bash
@@ -1,7 +1,9 @@
 # This script is used to deploy REST API by Render
 
 # Active environment variables in .env file (useful if we run the script locally)
-export $(cat .env | xargs) >/dev/null 2>&1
+if [ -f ".env" ]; then
+    export $(cat .env | xargs) >/dev/null 2>&1
+fi
 
 echo "Will run tests of REST API"
 npm run test
@@ -19,7 +21,7 @@ DOCUMENTATION_STATUS=`curl --write-out %{http_code} --silent --output /dev/null 
     -H "Authorization: Bearer $SWAGGER_TOKEN" -H "Content-Type: application/yaml" \
     --data-binary @documentation.yaml $SWAGGER_URL`
 
-if ( $DOCUMENTATION_STATUS < 200 || 299 < $DOCUMENTATION_STATUS ); then
+if [ $DOCUMENTATION_STATUS < 200 || 299 < $DOCUMENTATION_STATUS ]; then
     echo "\nAn error occured during documentation deployment...\n"
     exit 1
 fi
