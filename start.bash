97a1bfcd83a2c6da5bcfeefa075d8001bfb40e72
Florent TORNICELLI <florent@geode.com>
2023-05-15 15:42:43 +0200

    chore: add npm install in start script

--- start.bash
+++ start.bash
@@ -1,3 +1,7 @@
 # This script run Web App & REST API at the same time
-cd restAPI && ./start.bash > /dev/null && cd - &
-cd webApp && ./start.bash
+
+# Run REST API in the background
+cd restAPI && npm install > /dev/null && ./start.bash > /dev/null && cd - &
+
+# Run webApp
+cd webApp && npm install > /dev/null && ./start.bash
